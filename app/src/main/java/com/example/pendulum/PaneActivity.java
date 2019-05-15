3360ccfbdb8e68ca06726709aa1705842c5cd474
potentialize <38586343+potentialize@users.noreply.github.com>
2019-05-15 14:35:31 +0200

    Add layout of paneActivity

--- app/src/main/java/com/example/pendulum/PaneActivity.java
+++ app/src/main/java/com/example/pendulum/PaneActivity.java
@@ -11,6 +11,11 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+// deps for recyclerView spacing, see: https://www.androidhive.info/2016/05/android-working-with-card-view-and-recycler-view/
+import android.graphics.Rect;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
 import java.util.ArrayList;
 
 public class PaneActivity extends AppCompatActivity {
@@ -26,6 +31,9 @@ public class PaneActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pane);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // set title (default: dashboard)
+        this.setTitle(getResources().getString(R.string.activity_pane_title));
 
         // recycler
         initTileData();
@@ -43,15 +51,31 @@ public class PaneActivity extends AppCompatActivity {
     }
 
     private void initTileData() {
-        mTileTexts.add("Hello World!");
-        mTileTexts.add("Bruno");
-        mTileTexts.add("awesome");
+        mTileTexts.add("Study");
+        mTileTexts.add("Eat");
+        mTileTexts.add("Sleep");
+        mTileTexts.add("Sport");
+        mTileTexts.add("Game");
+//        mTileTexts.add("Hello World!");
+//        mTileTexts.add("Bruno");
+//        mTileTexts.add("awesome");
+//        mTileTexts.add("Very long text option to test what the layout does in extreme cases. Let's make it even longer and longer and longer and longer and longer...");
+//        mTileTexts.add("Hello World!");
+//        mTileTexts.add("Bruno");
+//        mTileTexts.add("awesome");
+//        mTileTexts.add("Very long text option to test what the layout does in extreme cases. Let's make it even longer and longer and longer and longer and longer...");
+//        mTileTexts.add("Hello World!");
+//        mTileTexts.add("Bruno");
+//        mTileTexts.add("awesome");
+//        mTileTexts.add("Very long text option to test what the layout does in extreme cases. Let's make it even longer and longer and longer and longer and longer...");
     }
 
     private void initRecyclerView() {
+        int spanCount = 2;
         recyclerView = (RecyclerView) findViewById(R.id.pane_recycler);
         recyclerView.setAdapter(new PaneRecyclerAdapter(this, mTileTexts));
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, spanCount));
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(spanCount));
         recyclerView.setHasFixedSize(true); // content does not change layout size -> improve performance
     }
 
@@ -75,5 +99,33 @@ public class PaneActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacingX;
+        private int spacingY;
+        private int marginFix;
+
+        public GridSpacingItemDecoration(int spanCount) {
+            this.spanCount = spanCount;
+            this.spacingX = (int) getResources().getDimension(R.dimen.tile_space_between_horizontal);
+            this.spacingY = (int) getResources().getDimension(R.dimen.tile_space_between_vertical);
+            this.marginFix = (int) getResources().getDimension(R.dimen.tile_margin_fix);
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int itemCount = parent.getAdapter().getItemCount();
+            int column = position % spanCount; // item column
+
+            outRect.left = (column == 0) ? (spacingX - marginFix) : (spacingX / 2 - marginFix);
+            outRect.right = (column == spanCount - 1) ? (spacingX - marginFix) : (spacingX / 2 - marginFix);
+
+            outRect.top = (position < spanCount) ? (spacingY - marginFix) : (spacingY / 2 - marginFix);
+            outRect.bottom = (itemCount - position <= (itemCount - 1) % spanCount + 1) ? (spacingY - marginFix) : (spacingY / 2 - marginFix);
+        }
     }
 }
